dbec5f5f3df6b848ec273293fa2e872d1b66fd85
Choyon Bonik <choyonbonik@gmail.com>
2023-02-14 17:13:00 +0600

    done

--- lib/pages/country_scroll_page.dart
+++ lib/pages/country_scroll_page.dart
@@ -82,56 +82,12 @@ class _CountryScrollPageState extends State<CountryScrollPage> {
         ]
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: (){
+          print(currentCountry);
+        },
         backgroundColor: Color(0xffbe047d),
         child: Icon(Icons.arrow_forward),
       )
-      // InkWell(
-      //   onTap: () {
-      //     Navigator.push(context, MaterialPageRoute(builder: (value){
-      //       return ImageUploadPage();
-      //     }));
-      //   },
-      //   child:
-      //   Container(
-      //     width: 100,
-      //     height: 46,
-      //     decoration: BoxDecoration(
-      //       borderRadius: BorderRadius.circular(6),
-      //       gradient: const LinearGradient(
-      //         begin: Alignment.topCenter,
-      //         end: Alignment.bottomCenter,
-      //         colors: [Color(0xffe90d65), Color(0xffac0087)],
-      //       ),
-      //       boxShadow: [
-      //         BoxShadow(
-      //           color: Colors.grey,
-      //           offset: const Offset(0.0, 1.0),
-      //           blurRadius: 6.0,
-      //         ),
-      //       ],
-      //     ),
-      //     child: Row(
-      //       mainAxisSize: MainAxisSize.min,
-      //       mainAxisAlignment: MainAxisAlignment.center,
-      //       crossAxisAlignment: CrossAxisAlignment.center,
-      //       children: [
-      //         AutoSizeText(
-      //           "Next",
-      //           textAlign: TextAlign.center,
-      //           textScaleFactor: textScaleFactor,
-      //           maxLines: 1,
-      //           overflow: TextOverflow.ellipsis,
-      //           style: TextStyle(
-      //             color: Colors.white,
-      //             fontSize: 16,
-      //             fontFamily: "Poppins SemiBold",
-      //           ),
-      //         ),
-      //       ],
-      //     ),
-      //   ),
-      // ),
     );
   }
 }
