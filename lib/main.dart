c50b6e450643b704b607f73cea790d84068a51e2
Choyon Bonik <choyonbonik@gmail.com>
2023-03-04 14:17:58 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:abedok_police_clearence_page/pages/dashboard_page.dart';
 import 'package:abedok_police_clearence_page/pages/home_page.dart';
 import 'package:abedok_police_clearence_page/pages/image_upload_page.dart';
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ImageUploadPage(),
+      home: HomePage(),
     );
   }
 }
